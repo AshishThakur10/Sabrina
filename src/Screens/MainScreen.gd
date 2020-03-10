cdb1b1f252e9ea55c98ee3ff95a3c31c57515d61
Ashish <sendashishthakur@gmail.com>
2020-03-10 21:36:19 +0530

    MainScreen script
    MainScreen script for menu tasks

---
+++ src/Screens/MainScreen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _ready() -> void:
+	SOUNDS.find_node("musice").play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
