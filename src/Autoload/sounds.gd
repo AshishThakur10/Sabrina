6b30cebf89ef1e8ca1ec5d6f89d1193f67239af8
Ashish <sendashishthakur@gmail.com>
2020-03-11 12:02:13 +0530

    Sounds script
    it handels all the sounds of the game like  looping...

---
+++ src/Autoload/sounds.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_musice_finished() -> void:
+	$"/root/SOUNDS".find_node("musice").play()
+	pass # Replace with function body.
